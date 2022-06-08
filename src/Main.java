9f6b55583e0bad11575a0a4644effc5137146272
Дэн заборов <denzaborov@Dens-Air.lan>
2022-06-08 23:45:57 +0300

    mistakes fixed

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,33 @@
 
 public class Main {
     public static void main(String[] args) {
-        boolean right = false;
-        if(args.length == 0 || args[0].equals("byte")  || args[0].equals("")) {
-            System.out.println("byte " +Byte.MAX_VALUE + " " + Byte.MIN_VALUE);
-            right = true;
-        }
-
-        if(args.length == 0 || args[0].equals("int")  || args[0].equals("")) {
+        if(args.length == 0){
+            System.out.println("byte " + Byte.MAX_VALUE + " " + Byte.MIN_VALUE);
             System.out.println("int " + Integer.MAX_VALUE + " " + Integer.MIN_VALUE);
-            right = true;
-        }
-
-        if(args.length == 0 || args[0].equals("short")  || args[0].equals("")) {
             System.out.println("short " + Short.MAX_VALUE + " " + Short.MIN_VALUE);
-            right = true;
-        }
-
-        if(args.length == 0 || args[0].equals("long")  || args[0].equals("")) {
             System.out.println("long " + Long.MAX_VALUE + " " + Long.MIN_VALUE);
-            right = true;
-        }
-
-        if(args.length == 0 || args[0].equals("char")  || args[0].equals("")) {
             System.out.println("char " + Character.MAX_VALUE + " " + Character.MIN_VALUE);
-            right = true;
-        }
-
-        if(args.length == 0 || args[0].equals("float")  || args[0].equals("")) {
-            System.out.println("float "+ Float.MAX_VALUE + " " + Float.MIN_VALUE);
-            right = true;
-        }
-
-        if(args.length == 0 || args[0].equals("double")  || args[0].equals("")) {
+            System.out.println("float " + Float.MAX_VALUE + " " + Float.MIN_VALUE);
             System.out.println("double " + Double.MAX_VALUE + " " + Double.MIN_VALUE);
-            right = true;
         }
-        if(!right)
-            System.out.println("<" + args[0] + ">" + " wrong argument");
+        for (int i = 0; i < args.length; i++) {
+
+            if (args[i].equals("byte"))
+                System.out.println("byte " + Byte.MAX_VALUE + " " + Byte.MIN_VALUE);
+            else if (args[i].equals("int"))
+                System.out.println("int " + Integer.MAX_VALUE + " " + Integer.MIN_VALUE);
+            else if (args[i].equals("short"))
+                System.out.println("short " + Short.MAX_VALUE + " " + Short.MIN_VALUE);
+            else if (args[i].equals("long"))
+                System.out.println("long " + Long.MAX_VALUE + " " + Long.MIN_VALUE);
+            else if (args[i].equals("char"))
+                System.out.println("char " + Character.MAX_VALUE + " " + Character.MIN_VALUE);
+            else if (args[0].equals("float"))
+                System.out.println("float " + Float.MAX_VALUE + " " + Float.MIN_VALUE);
+            else if (args[i].equals("double"))
+                System.out.println("double " + Double.MAX_VALUE + " " + Double.MIN_VALUE);
+            else
+                System.out.println("<" + args[0] + ">" + " wrong argument");
+        }
     }
 }
